816ccd0f50a48f2c0aa3bcaf685f0f427ba3925a
Brian Michel <brian.michel@gmail.com>
2014-03-09 22:28:20 -0400

    consolidating some logic

--- TumblrTest/Source/BSMPhotoContentView.m
+++ TumblrTest/Source/BSMPhotoContentView.m
@@ -22,7 +22,6 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
         self.imageView = [[UIImageView alloc] init];
         self.imageView.clipsToBounds = YES;
         
@@ -35,11 +34,9 @@
     [super didMoveToSuperview];
     if (self.superview) {
         BSMPhotoPost *photoPost = [self photoPost];
-        BSMPhoto *photo = [photoPost.photos firstObject];
-        if (photo) {
-            NSArray *sizes = photo.sizes;
-            BSMPhotoSize *firstSize = sizes[1];
-            [self.imageView bsm_loadImageAtURLAndFlash:firstSize.URL];
+        BSMPhotoSize *size = [self photoSizeForPhoto:photoPost];
+        if (size) {
+            [self.imageView bsm_loadImageAtURLAndFlash:size.URL];
         }
     }
 }
@@ -54,16 +51,28 @@
     [super updateFrameAfterSettingPost];
     
     BSMPhotoPost *photoPost = [self photoPost];
-    BSMPhoto *photo = [photoPost.photos firstObject];
-    if (photo) {
-        NSArray *sizes = photo.sizes;
-        BSMPhotoSize *firstSize = sizes[1];
-        NSInteger width = [firstSize.width integerValue];
-        NSInteger height = [firstSize.height integerValue];
+    BSMPhotoSize *photoSize = [self photoSizeForPhoto:photoPost];
+    
+    if (photoSize) {
+        NSInteger width = [photoSize.width integerValue];
+        NSInteger height = [photoSize.height integerValue];
         
         CGSize size = [self sizeForImageWithSize:CGSizeMake(width, height) viewSize:CGSizeMake(250.0, 300)];
         self.frame = CGRectMake(0, 0, size.width, size.height);
     }
+}
+
+- (BSMPhotoSize *)photoSizeForPhoto:(BSMPhotoPost *)photo {
+    BSMPhoto *selectedPhoto = [photo.photos firstObject];
+    if (selectedPhoto) {
+        NSArray *sizes = selectedPhoto.sizes;
+        //TODO there's probably a better hueristic for this
+        //could possibly hoist the size up to form a key?
+        if ([sizes count] >= 2) {
+            return sizes[1];
+        }
+    }
+    return nil;
 }
 
 - (BSMPhotoPost *)photoPost {
